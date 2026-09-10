e5703796e177cd222816244a8e4714650a36cf0a
cobrajt <rumert01@gmail.com>
2026-09-10 13:27:05 -0400

    Limpia caches antiguos de la PWA

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'dulus-gym-track-v5';
+const CACHE = 'dulus-gym-track-v6';
 const ASSETS = ['./', './index.html', './styles.css', './library.css', './workouts.css', './storage.js', './exercises.js', './app.js', './library.js', './workouts.js', './manifest.webmanifest', './icons/icon-192.svg', './icons/icon-512.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
